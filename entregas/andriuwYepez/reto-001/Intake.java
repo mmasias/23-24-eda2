f32c0b1d3f2f3995ecc11f49135c647e8c88ca50
andriuw08 <andriuw.yepez@gmail.com>
2024-02-21 17:49:02 +0100

    Codigo finalizado

--- entregas/andriuwYepez/reto-001/Intake.java
+++ entregas/andriuwYepez/reto-001/Intake.java
@@ -24,31 +24,51 @@ class Intake {
         System.out.println(this.toString());
     }
 
-    public void createIntakeFunctionality(String foodName) {
-        Food food = new Food(foodName);
-        addFood(food);
-    }
-
     public void createIntake() {
         boolean creating = true;
         Scanner userInput = new Scanner(System.in);
         while (creating) {
-            System.out.println("Nombre del alimento (-1 para terminar)");
+            System.out.println("Nombre del alimento por crear (-1 para terminar)");
             String foodName = userInput.nextLine();
             if (foodName.equals("-1")) {
                 creating = !creating;
             } else {
-                createIntakeFunctionality(foodName);
+                Food food = new Food(foodName);
+                addFood(food);
             }
         }
     }
 
-    public void deleteIntakeFunctionality(Food current, Food previous) {
-        if (previous == null) {
-            first = current.getNext();
-        } else {
-            previous.setNext(current.getNext());
+    public void deleteIntakeFunctionality(FoodNode nodeToDelete) {
+        if (nodeToDelete == null) {
+            return;
         }
+        
+        FoodNode previous = null;
+        FoodNode current = first;
+        
+        while (current != null && current != nodeToDelete) {
+            previous = current;
+            current = current.getNext();
+        }
+        
+        if (current == null) {
+            return;
+        }
+        
+
+        if (previous != null) {
+            previous.setNext(current.getNext());
+        } else {
+            first = current.getNext();
+        }
+    }
+
+    public void editIntakeFunctionality(FoodNode node) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Ingresa el nuevo alimento");
+        String newFoodName = userInput.nextLine();
+        node.getFood().setName(newFoodName);
     }
 
     public void deleteAllIntake(){
@@ -58,69 +78,69 @@ class Intake {
     public void deleteIntake() {
         boolean deleteIntake = true;
         Scanner userInput = new Scanner(System.in);
+        
         while (deleteIntake) {
             System.out.println("1) Eliminar un alimento");
             System.out.println("2) Eliminar todos los alimentos");
-            System.out.println("3) Guardar" + "\n");
+            System.out.println("3) Guardar Alimentos\n");
             String o = userInput.nextLine();
-            if(o.equals("1")) {
+            
+            if (o.equals("1")) {
                 FoodNode current = first;
-                FoodNode previous = null;
                 while (current != null) {
                     System.out.println("El alimento actual es: " + current.getFood().toString());
                     System.out.println("1) Eliminar");
-                    System.out.println("2) Siguiente" + "\n");
+                    System.out.println("2) Siguiente\n");
                     String optionDelete = userInput.nextLine();
-
-                    if(optionDelete.equals("1")) {
+    
+                    if (optionDelete.equals("1")) {
                         System.out.println("Eliminando un alimento");
-                        deleteIntakeFunctionality(current, previous);
-                    } else if(optionDelete.equals("2")) {
-                        previous = current;
-                        current = current.getNext();
+                        deleteIntakeFunctionality(current);
+                        break;
+                    } else if (optionDelete.equals("2")) {
+                        System.out.println("Siguiente alimento\n");
                     }
+                    current = current.getNext();
                 }
             } else if (o.equals("2")) {
-                System.out.println("Eliminando todos");
+                System.out.println("Eliminando todos los alimentos");
                 deleteAllIntake();
             } else if (o.equals("3")) {
-                deleteIntake = !deleteIntake;
+                deleteIntake = false;
             }
         }
     }
 
-    // public void editIntake() {
-    //     boolean editing = true;
-    //     Scanner userInput = new Scanner(System.in);
-
-    //     while (editing) {
-    //         System.out.println("1) Editar alimentos");
-    //         System.out.println("2) Guardar Alimentos" + "\n");
-    //         String o = userInput.nextLine();
-    //         if(o.equals("1")) {
-    //             FoodNode current = first;
-
-    //             while (current != null) {
-    //                 System.out.println("El alimento actual es: " + current.getFood().toString());
-    //                 System.out.println("1) Editar");
-    //                 System.out.println("2) Siguiente" + "\n");
-    //                 String optionEditing = userInput.nextLine();
-    //                 if(optionEditing.equals("1")) {
-    //                     System.out.println("Editando");
-    //                     deleteIntake();
-    //                     createIntake();
-    //                 } else if(optionEditing.equals("2")){
-    //                     System.out.println("Guardado exitosamente!");
-    //                     Food food = current.getFood();
-    //                     addFood(food);
-    //                 }
-    //                 current = current.getNext();
-    //             }
-    //         } else if (o.equals("2")) {
-    //             editing = !editing;
-    //         }
-    //     }
-    // }
+    public void editIntake() {
+        boolean editing = true;
+        Scanner userInput = new Scanner(System.in);
+        
+        while (editing) {
+            System.out.println("1) Editar alimentos");
+            System.out.println("2) Guardar Alimentos\n");
+            String o = userInput.nextLine();
+            
+            if (o.equals("1")) {
+                FoodNode current = first;
+                while (current != null) {
+                    System.out.println("El alimento actual es: " + current.getFood().toString());
+                    System.out.println("1) Editar");
+                    System.out.println("2) Siguiente\n");
+                    String optionEditing = userInput.nextLine();
+                    
+                    if (optionEditing.equals("1")) {
+                        System.out.println("Editando");
+                        editIntakeFunctionality(current);
+                    } else if (optionEditing.equals("2")) {
+                        System.out.println("Guardado exitosamente!");
+                    }
+                    current = current.getNext();
+                }
+            } else if (o.equals("2")) {
+                editing = false;
+            }
+        }
+    }
 
 
     @Override
@@ -138,12 +158,9 @@ class Intake {
 
         Intake breakfast = new Intake();
         breakfast.createIntake();
-        // breakfast.editIntake();
+        breakfast.editIntake();
         breakfast.deleteIntake();
         breakfast.printFoodListing();
 
     }
 }
-
-// tengo que dejar ahi solo las funcionalides y los textos en otro porque si reutilizo funciones me saldra os textos siempre
-// tambien se ejecuta repetidamente, no hasta que se acaben los elementos
