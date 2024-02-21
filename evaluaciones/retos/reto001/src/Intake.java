d8edaf58c66dcccd4833caa89d081eb1d724afd7
luciaZama <lzamanillol@gmail.com>
2024-02-21 13:55:49 +0100

    initial commit

---
+++ evaluaciones/retos/reto001/src/Intake.java
@@ -0,0 +1,132 @@
+import java.util.Scanner;
+
+class Intake {
+    
+     private FoodNode first;
+
+    public Intake() {
+        first = null;
+    }
+
+    public void addFood(Food food) {
+        FoodNode newFoodNode = new FoodNode(food);
+        if (first == null) {
+            first = newFoodNode;
+        } else {
+            FoodNode current = first;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(newFoodNode);
+        }
+    }
+
+    public void printFoodListing() {
+        System.out.println(this.toString());
+    }
+
+    public void createIntake() {
+        boolean creating = true;
+        Scanner userInput = new Scanner(System.in);
+        while (creating) {
+            System.out.println("Nombre del alimento por crear (-1 para terminar)");
+            String foodName = userInput.nextLine();
+            if (foodName.equals("-1")) {
+                creating = !creating;
+            } else {
+                Food food = new Food(foodName);
+                addFood(food);
+            }
+        }
+    }
+
+    private void editIntake() {
+        boolean editing = true;
+        Scanner userInput = new Scanner(System.in);
+        while (editing) {
+            System.out.println("Nombre del alimento por editar (-1 para terminar)");
+            String foodName = userInput.nextLine();
+            if (foodName.equals("-1")) {
+                editing = !editing;
+            } else {
+                editFood(foodName);
+            }
+        }
+    }
+
+    private void deleteIntake() {
+        Scanner userInput = new Scanner(System.in);
+        boolean deleting = true;
+        while (deleting) {
+            System.out.println("Nombre del alimento por eliminar (-1 para terminar)");
+            String foodName = userInput.nextLine();
+            if (foodName.equals("-1")) {
+                deleting = !deleting;
+            } else {
+                removeFood(foodName);
+            }
+        }
+    }
+
+    private void removeFood(String foodName) {
+        if (first.getFood().getName().equals(foodName)) {
+            first = first.getNext();
+            return;
+        } else {
+            FoodNode current = first;
+            while (current.getNext() != null && !current.getNext().getFood().getName().equals(foodName)) {
+                current = current.getNext();
+            }
+            if (current.getNext() != null) {
+                current.setNext(current.getNext().getNext());
+            }
+        }
+    }
+
+    private void editFood(String foodName) {
+        Scanner userInput = new Scanner(System.in);
+        FoodNode current = first;
+        while (current != null) {
+            if (current.getFood().getName().equals(foodName)) {
+                System.out.println("Nuevo nombre de [" + foodName + "]");
+                String newFoodName = userInput.nextLine();
+                current.getFood().setName(newFoodName);
+                return;
+            }
+            current = current.getNext();
+        }
+        System.out.println("Alimento no encontrado");
+    }
+
+    private void deleteAllIntake() {
+        first = null;
+    }
+
+    @Override
+    public String toString() {
+        String foodListing = "";
+        FoodNode current = first;
+        while (current != null) {
+            foodListing = foodListing + current.getFood().toString() + "\n";
+            current = current.getNext();
+        }
+        return foodListing;
+    }
+
+    public static void main(String[] args) {
+
+        Intake breakfast = new Intake();
+
+        breakfast.createIntake();
+        breakfast.printFoodListing();
+
+        breakfast.editIntake();
+        breakfast.printFoodListing();
+
+        breakfast.deleteIntake();
+        breakfast.printFoodListing();
+
+        breakfast.deleteAllIntake();
+        breakfast.printFoodListing();
+    }
+}
