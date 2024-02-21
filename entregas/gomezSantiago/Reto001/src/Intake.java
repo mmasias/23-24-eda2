0e408b26917165aee782eb264a730f421414e2ed
Santiago Gomez de la Torre <gomezdelatorre15@gmail.com>
2024-02-21 23:32:09 +0100

    Finish project

--- entregas/gomezSantiago/Reto001/src/Intake.java
+++ entregas/gomezSantiago/Reto001/src/Intake.java
@@ -19,6 +19,25 @@ class Intake {
             current.setNext(newFoodNode);
         }
     }
+    public void deleteFood(String foodName){
+        FoodNode current = first;
+        FoodNode previous = null;
+        while (current != null) {
+            if (current.getFood().getName().equals(foodName)) {
+                if (previous == null) {
+                    first = current.getNext();
+                } else {
+                    previous.setNext(current.getNext());
+                }
+                return;
+            }
+            previous = current;
+            current = current.getNext();
+        }
+        System.out.println("+---------------------------+");
+        System.out.println("| Se ha eliminado la comida |");
+        System.out.println("+---------------------------+");
+    }
 
     public void printFoodListing() {
         System.out.println(this.toString());
@@ -27,13 +46,10 @@ class Intake {
     public void createIntake() {
         boolean creating = true;
         Scanner userInput = new Scanner(System.in);
-        System.out.println("+--------------------------+");
-        System.out.println("| Creación de una Ingesta: |");
-        System.out.println("+--------------------------+");
         while (creating) {
-            System.out.println("+----------------------------------------+");
-            System.out.println("| Nombre del alimento (-1 para terminar) |");
-            System.out.println("+----------------------------------------+");
+            System.out.println("+-----------------------------------------+");
+            System.out.println("| Nombre del alimento (-1 para terminar): |");
+            System.out.println("+-----------------------------------------+");
             String foodName = userInput.nextLine();
             if (foodName.equals("-1")) {
                 creating = !creating;
@@ -44,32 +60,33 @@ class Intake {
         }
     }
 
-    public void modifyIntake() {
-        boolean modifying = true;
-        Scanner userInput = new Scanner(System.in);
-        System.out.println("+--------------------------------+");
-        System.out.println("| Modificacion de alimentos      |");
-        System.out.println("+--------------------------------+");
-        while (modifying) {
-            System.out.println("+--------------------------------+");
-            System.out.println("| Alimento a modificar: " + " |");
-            System.out.println("+--------------------------------+");
-            String newFoodName = userInput.nextLine();
-            if (newFoodName.equals("-1")) {
-                modifying = !modifying;
-            } else {
-                Food food = new Food(newFoodName);
-                addFood(food);
-            }
-        }
-        printFoodListing();
+    public void deleteIntake(){
+        System.out.println("+-----------------------------------------+");
+        System.out.println("| Ingrese el alimento que desea eliminar: |");
+        System.out.println("+-----------------------------------------+");
+        String foodNameDelete = new Scanner(System.in).nextLine();
+        System.out.println("+-----------------------------------------+");
+        System.out.println("| Se ha eliminado " + foodNameDelete + " correctamente |" + "\n");
+        System.out.println("+-----------------------------------------+");
+        deleteFood(foodNameDelete);
     }
+    public void editIntake(){
+        System.out.println("+---------------------------------------+");
+        System.out.println("| Ingrese el alimento que desea editar: |");
+        System.out.println("+---------------------------------------+");
+        String foodNameEdit = new Scanner(System.in).nextLine();
+        System.out.println("+--------------------------------------------------+");
+        System.out.println("| Se ha editado la comida: " + foodNameEdit + " |"+ "\n");
+        System.out.println("+--------------------------------------------------+");
+        deleteFood(foodNameEdit);
 
-    public void deleteIntake() {
-        System.out.println("Eliminar alimentos...");
-        printFoodListing();
+        System.out.println("+---------------------------------------+");
+        System.out.println("| Ingrese el nombre del nuevo alimento: |");
+        System.out.println("+---------------------------------------+");
+        String foodNameNew = new Scanner(System.in).nextLine();
+        Food food = new Food(foodNameNew);
+        addFood(food);
     }
-
     @Override
     public String toString() {
         String foodListing = "";
@@ -85,9 +102,9 @@ class Intake {
 
         Intake breakfast = new Intake();
         breakfast.createIntake();
-        breakfast.printFoodListing();
-        breakfast.modifyIntake();
+        breakfast.editIntake();
         breakfast.deleteIntake();
+        breakfast.printFoodListing();
 
     }
 }
