d6dff9f120b279b5a900067231373680e0eda916
Dani Puente <danielpuentesaranana@gmail.com>
2024-02-21 18:05:07 +0100

    Reto

---
+++ entregas/puenteDaniel/Reto001/DayNode.java
@@ -0,0 +1,27 @@
+package entregas.puenteDaniel.Reto001;
+
+public class DayNode {
+    private Day day;
+    private DayNode next;
+
+    public DayNode(Day day) {
+        this.day = day;
+        this.next = null;
+    }
+
+    public Day getDay() {
+        return day;
+    }
+
+    public void setDay(Day day) {
+        this.day = day;
+    }
+
+    public DayNode getNext() {
+        return next;
+    }
+
+    public void setNext(DayNode next) {
+        this.next = next;
+    }
+}
