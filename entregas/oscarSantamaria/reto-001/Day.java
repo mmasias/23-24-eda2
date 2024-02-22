bd4b2d97b69b8408f95acb0421482fa2755986c8
Oscar Santamaria <114479802+oscarsantasanchez@users.noreply.github.com>
2024-02-22 10:45:19 +0100

    Add files via upload

---
+++ entregas/oscarSantamaria/reto-001/Day.java
@@ -0,0 +1,95 @@
+
+import java.util.Scanner;
+
+class Day {
+    private IntakeNode first;
+
+    public Day() {
+        first = null;
+    }
+
+    public void addIntake(Intake intake) {
+        IntakeNode newIntakeNode = new IntakeNode(intake);
+        if (first == null) {
+            first = newIntakeNode;
+        } else {
+            IntakeNode current = first;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(newIntakeNode);
+        }
+    }
+
+    public void printIntakeListing() {
+        System.out.println(this.toString());
+    }
+
+    public void createIntakes() {
+        boolean creating = true;
+        Scanner userInput = new Scanner(System.in);
+        while (creating) {
+            System.out.println("Nombre de la ingesta (-1 para terminar)");
+            String intakeName = userInput.nextLine();
+            if (intakeName.equals("-1")) {
+                creating = !creating;
+            } else {
+                Intake intake = new Intake();
+                intake.createIntake();
+                addIntake(intake);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        String intakeListing = "";
+        IntakeNode current = first;
+        while (current != null) {
+            intakeListing = intakeListing + current.getIntake().toString() + "\n";
+            current = current.getNext();
+        }
+        return intakeListing;
+    }
+
+    public boolean updateIntake(int position, Intake newIntake) {
+        IntakeNode current = first;
+        int count = 0;
+        while (current != null) {
+            if (count == position) {
+                current.setIntake(newIntake);
+                return true;
+            }
+            current = current.getNext();
+            count++;
+        }
+        return false;
+    }
+
+    public boolean deleteIntake(int position) {
+        IntakeNode current = first;
+        IntakeNode previous = null;
+        int count = 0;
+        while (current != null) {
+            if (count == position) {
+                if (previous == null) {
+                    first = current.getNext();
+                } else {
+                    previous.setNext(current.getNext());
+                }
+                return true;
+            }
+            previous = current;
+            current = current.getNext();
+            count++;
+        }
+        return false;
+    }
+    
+    public static void main(String[] args) {
+
+        Day monday = new Day();
+        monday.createIntakes();
+        monday.printIntakeListing();
+    }
+}
