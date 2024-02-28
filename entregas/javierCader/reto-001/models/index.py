4712ce45eb297da2fbeb331cc52212c0b54dfd71
Hexcss <cadersuay.javier@gmail.com>
2024-02-28 11:40:22 +0100

    feat: Finished CRUD

--- entregas/javierCader/reto-001/models/index.py
+++ entregas/javierCader/reto-001/models/index.py
@@ -6,7 +6,7 @@ class Food:
 class Intake: 
     def __init__(self, type: str):
         self.type = type
-        self.foods = CustomList()
+        self.foods = []
 
     def add_food(self, food: Food):
         self.foods.append(food)
@@ -14,17 +14,17 @@ class Intake:
 class Day: 
     def __init__(self, number: int):
         self.number = number
-        self.intakes = CustomList([Intake("Breakfast"), Intake("Mid-morning"), Intake("Lunch"), Intake("Snack"), Intake("Dinner")])
+        self.intakes = [Intake("Breakfast"), Intake("Mid-morning"), Intake("Lunch"), Intake("Snack"), Intake("Dinner")]
 
 class Survey:
     def __init__(self):
-        self.days = CustomList([Day(i) for i in range(1, 7)])
+        self.days = [Day(i) for i in range(1, 7)]
 
 class Patient:
     def __init__(self, name: str, discharge_date: str):
         self.name = name
         self.discharge_date = discharge_date
-        self.surveys = CustomList()
+        self.surveys = []
 
     def add_survey(self, survey: Survey):
         self.surveys.append(survey)
