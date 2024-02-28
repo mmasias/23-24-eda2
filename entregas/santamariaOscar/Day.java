2aa95e8b485136fd999ebac47939f3f183b64261
Oscar Santamaria <114479802+oscarsantasanchez@users.noreply.github.com>
2024-02-28 15:55:16 +0100

    Update Day.java

--- entregas/santamariaOscar/Day.java
+++ entregas/santamariaOscar/Day.java
@@ -2,20 +2,28 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 class Day {
-    private ArrayList<Intake> intakes;
+    private ArrayList<Intake> intakeList;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public  void setName(String name) {
+        this.name = name;
+    }
 
     public Day() {
-        intakes = new ArrayList<>();
+        intakeList = new ArrayList<Intake>();
+
     }
 
     public void addIntake(Intake intake) {
-        intakes.add(intake);
+        intakeList.add(intake);
     }
 
     public void printIntakeListing() {
-        for (Intake intake : intakes) {
-            System.out.println(intake);
-        }
+        System.out.println(this.toString());
     }
 
     public void createIntakes() {
@@ -23,37 +31,67 @@ class Day {
         Scanner userInput = new Scanner(System.in);
         while (creating) {
             System.out.println("Nombre de la ingesta (-1 para terminar)");
-            String intakeName = userInput.nextLine();
-            if (intakeName.equals("-1")) {
-                creating = false;
+            String dayName = userInput.nextLine();
+            if (dayName.equals("-1")) {
+                creating = !creating;
             } else {
                 Intake intake = new Intake();
+                intake.setName(dayName);
                 intake.createIntake();
                 addIntake(intake);
             }
         }
-        userInput.close();
     }
 
-    public boolean updateIntake(int position, Intake newIntake) {
-        if (position >= 0 && position < intakes.size()) {
-            intakes.set(position, newIntake);
-            return true;
+    @Override
+    public String toString() {
+        String intakeListing = name+"\n";
+        for (Intake intake : intakeList) {
+            intakeListing = intakeListing + intake.toString() + "\n";
         }
-        return false;
+        return intakeListing;
     }
 
-    public boolean deleteIntake(int position) {
-        if (position >= 0 && position < intakes.size()) {
-            intakes.remove(position);
-            return true;
-        }
-        return false;
+    public void deleteIntakes() {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Nombrame el nombre de la ingesta que quieres eliminar");
+        String intakeName = userInput.nextLine();
+        for(int i=0; i<this.intakeList.size();i++){
+            if (this.intakeList.get(i).getName().equalsIgnoreCase(intakeName)) {
+                this.intakeList.remove(i);
+            }
+        }        
+    }
+
+    public void updateIntakes() {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Nombrame el nombre de la ingesta que quieres actualizar");
+        String intakeName = userInput.nextLine();
+        for(int i=0; i<this.intakeList.size();i++){
+            if (this.intakeList.get(i).getName().equalsIgnoreCase(intakeName)) {
+                System.out.println("Nombrame el nombre de la ingesta que quieres añadir");
+                String newintakeName = userInput.nextLine();
+
+                Intake intake= new Intake();
+                intake.setName(newintakeName);
+                intake.createIntake(); 
+                
+                intakeList.set(i, intake);
+
+            }
+        }       
+
     }
 
     public static void main(String[] args) {
+
         Day monday = new Day();
         monday.createIntakes();
         monday.printIntakeListing();
-    }
+        monday.deleteIntakes();
+        monday.printIntakeListing();
+        monday.updateIntakes();
+        monday.printIntakeListing();
+    }  
+
 }
