8d251d20a125b76ab1beac8cdff2fd029de26267
sara-patino <sara.patino@alumnos.uneatlantico.es>
2024-03-06 23:08:43 +0100

    plantuml

---
+++ entregas/saraPatino/003/src/Titulo.java
@@ -0,0 +1,20 @@
+package src; 
+
+class Titulo {
+    private static int contadorIds = 0;
+    private int id;
+    private String name;
+
+    public Titulo(String name) {
+        this.id = ++contadorIds;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
