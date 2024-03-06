d6bc65a10d7f9ae45b1bb639a07b037ae94f61d9
luciaZama <lzamanillol@gmail.com>
2024-03-06 13:15:43 +0100

    clase Biblioteca creada

---
+++ evaluaciones/retos/reto003/src/Biblioteca.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Biblioteca {
+    private ArrayList<Documento> documentos = new ArrayList<>();
+
+    public Biblioteca(ArrayList<Documento> documentos) {
+        this.documentos = documentos;
+    }
+
+    
+
+
+}
