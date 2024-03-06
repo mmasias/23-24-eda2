6676ae0e566768509f727efbb508c8fc05e8f113
luciaZama <lzamanillol@gmail.com>
2024-03-06 12:59:38 +0100

    Clase abstracta Articulo creada

---
+++ evaluaciones/retos/reto003/src/Articulo.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Articulo extends Documento {
+
+    public Articulo (String titulo, ArrayList<String> autores, int anhoPublicacion, String tipoDocumento, ArrayList<String> palabrasClave) {
+        super(titulo, autores, anhoPublicacion, tipoDocumento, palabrasClave);
+    }
+
+    public void setTipoDocumento(String tipoDocumento) {
+        tipoDocumento = "Articulo";
+    }
+}
