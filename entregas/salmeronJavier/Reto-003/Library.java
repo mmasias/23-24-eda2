7be88a6ac8d28948abd4a935765ee65bc615306a
0xJVR <114479588+0xJVR@users.noreply.github.com>
2024-03-07 10:46:06 +0100

    Update Library to improve search features

--- entregas/salmeronJavier/Reto-003/Library.java
+++ entregas/salmeronJavier/Reto-003/Library.java
@@ -12,13 +12,56 @@ public class Library {
         documents.add(document);
     }
 
-    public Document findDocumentByTitle(String title) {
+    public Document searchDocumentByTitle(String title) {
         for (Document doc : documents) {
             if (doc.getTitle().equalsIgnoreCase(title)) {
                 return doc;
             }
         }
         return null;
+    }
+
+    public List<Document> searchByAuthor(String author) {
+        List<Document> results = new ArrayList<>();
+        for (Document doc : documents) {
+            for (String docAuthor : doc.getAuthors()) {
+                if (docAuthor.equalsIgnoreCase(author)) {
+                    results.add(doc);
+                    break;
+                }
+            }
+        }
+        return results;
+    }
+
+    public List<Document> searchByType(String type) {
+        List<Document> results = new ArrayList<>();
+        for (Document doc : documents) {
+            if (doc.getType().equalsIgnoreCase(type)) {
+                results.add(doc);
+            }
+        }
+        return results;
+    }
+
+    public List<Document> searchByYear(int year) {
+        List<Document> results = new ArrayList<>();
+        for (Document doc : documents) {
+            if (doc.getPublishingYear() == year) {
+                results.add(doc);
+            }
+        }
+        return results;
+    }
+    
+    public List<Document> searchByKeyword(String keyword) {
+        List<Document> results = new ArrayList<>();
+        for (Document doc : documents) {
+            if (doc.getKeyWords().contains(keyword)) {
+                results.add(doc);
+            }
+        }
+        return results;
     }
 
     public void updateDocument(String title, Document updatedDocument) {
@@ -29,20 +72,11 @@ public class Library {
                 return;
             }
         }
-        System.out.println("Document not found.");
+        // Change to return null or just ignore?
     }
 
     public void deleteDocument(String title) {
         documents.removeIf(doc -> doc.getTitle().equalsIgnoreCase(title));
     }
 
-    public List<Document> searchByKeyword(String keyword) {
-        List<Document> results = new ArrayList<>();
-        for (Document doc : documents) {
-            if (doc.getKeyWords().contains(keyword)) {
-                results.add(doc);
-            }
-        }
-        return results;
-    }
 }
--- entregas/salmeronJavier/Reto-003/Main.java
+++ entregas/salmeronJavier/Reto-003/Main.java
@@ -4,20 +4,56 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Library library = new Library();
+        
+        List<Document> searchResults;
+        
+        List<String> authors01 = new ArrayList<>();
+        authors01.add("Jose Luis");
+        authors01.add("Antonie Delmonte");
 
-        List<String> authors = new ArrayList<>();
-        authors.add("Jose Luis");
-        authors.add("Antonie Delmonte");
-
-        Document doc01 = new Document("Compiling gnu/linux for normies", authors, 2024, "Book");
+        Document doc01 = new Document("Compiling gnu/linux for normies", authors01, 2024, "Book");
         doc01.addKeyword("Linux");
         doc01.addKeyword("Technology");
         library.addDocument(doc01);
 
-        System.out.println("Searching keyword 'Linux':");
-        List<Document> searchResults = library.searchByKeyword("Linux");
-        for (Document doc : searchResults) {
-            System.out.println("Found: " + doc.getTitle() + "By: " + doc.getAuthors());
+        List<String> authors02 = new ArrayList<>();
+        authors02.add("Walter White");
+        
+        Document doc02 = new Document("Cooking for science", authors02, 2024, "Paper");
+        doc02.addKeyword("Science");
+        library.addDocument(doc02);
+
+        System.out.println("Searching document 'Compiling gnu':");
+        Document searchDocument = library.searchDocumentByTitle("Compiling gnu/linux for normies");
+        if (searchDocument != null) {
+            System.out.println(" - Document Found: ");
+            System.out.println(" - Title: " + searchDocument.getTitle());
+            System.out.println(" - Authors: " + String.join(", ", searchDocument.getAuthors()));
+            System.out.println(" - Publishing Year: " + searchDocument.getPublishingYear());
+            System.out.println(" - Type: " + searchDocument.getType());
+            System.out.println(" - Keywords: " + String.join(", ", searchDocument.getKeyWords()));
+        } else {
+            System.out.println("No document found");
         }
+   
+
+        System.out.println("Searching keyword 'Linux':");
+        searchResults = library.searchByKeyword("Linux");
+        for (Document doc : searchResults) {
+            System.out.println("Found: " + doc.getTitle() + " By: " + String.join(", ", doc.getAuthors()));
+        }
+
+        System.out.println("Searching year 2024:");
+        searchResults = library.searchByYear(2024);
+        for (Document doc : searchResults) {
+            System.out.println("Found: " + doc.getTitle() + " By: " + String.join(", ", doc.getAuthors()));
+        }
+
+        System.out.println("Searching books:");
+        searchResults = library.searchByType("paper");
+        for (Document doc : searchResults) {
+            System.out.println("Found: " + doc.getTitle() + " By: " + String.join(", ", doc.getAuthors()));
+        }
+
     }
 }
