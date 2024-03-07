c01efb9852f10752fd58895303187fc63e5cbd32
DavidGarciaCosta <david.garcia1@alumnos.uneatlantico.es>
2024-03-07 01:19:36 +0100

    cambios

---
+++ entregas/garciaDavid/reto003/src/clases/Main.java
@@ -0,0 +1,8 @@
+package clases;
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+        Gestion gestion = new Gestion(new ArrayList<Documento>());
+        gestion.menu();
+    }
+}
