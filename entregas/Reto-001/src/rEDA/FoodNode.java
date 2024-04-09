7375a19d9bd7ff15d8ba7f9bcd01789ef8754a27
Maura Martinez <137115639+Martinez-rosa@users.noreply.github.com>
2024-04-09 10:33:22 +0200

    Add files via upload

---
+++ entregas/Reto-001/src/rEDA/FoodNode.java
@@ -0,0 +1,30 @@
+package rEDA;
+public class FoodNode {
+    class FoodNode {
+
+        private Food food;
+        private FoodNode next;
+
+        public FoodNode(Food alimento) {
+            this.food = alimento;
+            this.next = null;
+        }
+
+        public Food getFood() {
+            return food;
+        }
+
+        public void setFood(Food food) {
+            this.food = food;
+        }
+
+        public FoodNode getNext() {
+            return next;
+        }
+
+        public void setNext(FoodNode next) {
+            this.next = next;
+        }
+    }
+}
+
