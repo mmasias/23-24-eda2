9ceffa8cc361d1a68e35520a3bf8e8deb88adf61
miguelgomez75 <miguel.gomez@alumnos.uneatlantico.es>
2024-04-12 10:42:18 +0200

    Creadas las clases HallarPalabra y HallarSuma

---
+++ entregas/gomezMiguel/RetoBigO/HallarSuma.java
@@ -0,0 +1,14 @@
+package RetoBigO;
+public class HallarSuma {
+    public static void main(String[] args) {
+        int[] numbers = {3, 2, 9, 8};
+        int numeroobjetivo = 10;
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == numeroobjetivo) {
+                    System.out.println("los números en los índices " + i + " y " + j + " suman " + numeroobjetivo);
+                }
+            }
+        }
+    }
+}
