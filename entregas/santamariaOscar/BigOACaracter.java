34eea184cc1c317ade3225397bab77ecc52aaf83
oscarsantasanchez <oscar.santamaria@alumnos.uneatlantico.es>
2024-04-15 11:19:34 +0200

    creacion BigOCaracter.java

--- entregas/santamariaOscar/BigOACaracter.java
+++ entregas/santamariaOscar/BigOACaracter.java
@@ -4,7 +4,7 @@ public class BigOACaracter {
    
     public static void main(String[] args) {
         char[] caracteres = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
-        String palabraClave = "deg";
+        String palabraClave = "eba";
         int[] resultado = encontrarPalabraClave(caracteres, palabraClave);
 
         if (resultado.length == 0) {
@@ -15,22 +15,13 @@ public class BigOACaracter {
     }
    
     public static int[] encontrarPalabraClave(char[] caracteres, String palabraClave) {
-        int[] indices = new int[caracteres.length];
-        int indice = 0;
+        int[] indices = new int[3];
+        String palabra= new String(caracteres);
 
-        for (int i = 0; i < caracteres.length; i++) {
-            if (caracteres[i] == palabraClave.charAt(indice)) {
-                indices[indice] = i;
-                indice++;
-                if (indice == palabraClave.length()) {
-                    return indices;
-                }
-            } else {
-                indice = 0;
-            }
+        for(int i=0; i<palabraClave.length();i++){
+            indices[i]=palabra.indexOf(palabraClave.charAt(i));
         }
-
-        return new int[0];
+        return indices;
     }
 
 }
