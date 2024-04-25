18ab650966b0bbd4cfe2773ce90c13b85a5a3cf2
javier.cader <javier.cader@ct.uneatlantico.es>
2024-04-25 12:57:52 +0200

    feat: Added Reto004

---
+++ entregas/javierCader/reto-001/src/Diet.java
@@ -0,0 +1,62 @@
+package vEDA.v003;
+
+import java.util.Scanner;
+
+class Diet {
+    private DayNode first;
+
+    public Diet() {
+        first = null;
+    }
+
+    public void addDay(Day day){
+        DayNode newDayNode = new DayNode(day);
+        if (first == null){
+            first = newDayNode;
+        } else {
+            DayNode current = first;
+            while(current.getNext()!=null){
+                current = current.getNext();
+            }
+            current.setNext(newDayNode);
+        }
+    }
+
+    public void printDayListing(){
+        System.out.println(this.toString());
+    }
+
+    public void createDays() {
+        boolean creating = true;
+        Scanner userInput = new Scanner(System.in);
+        while(creating){
+            System.out.println("Nombre del dia (-1 para terminar)");
+            String dayName = userInput.nextLine();
+            if(dayName.equals("-1")) {
+                creating=!creating;
+            } else {
+                Day day = new Day();
+                day.createIntakes();
+                addDay(day);
+            }
+        }        
+    }
+
+    @Override
+    public String toString() {
+        String daysListing = "";
+        DayNode current = first;
+        while (current != null) {
+            daysListing = daysListing + current.getDay().toString() + "\n";
+            current = current.getNext();
+        }
+        return daysListing;
+    }    
+
+    public static void main(String[] args) {
+
+        Diet diet = new Diet();
+        diet.createDays();
+        diet.printDayListing();
+    }      
+}
