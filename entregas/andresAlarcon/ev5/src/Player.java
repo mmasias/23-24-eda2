4ecd9ba6af6289203c26adf9ceea8f6262d7b486
AndrextA <andrextalarcon@gmail.com>
2024-04-25 04:16:43 +0200

    almost working challenge 5

---
+++ entregas/andresAlarcon/ev5/src/Player.java
@@ -0,0 +1,93 @@
+import java.util.HashMap;
+import java.util.List;
+
+public class Player {
+  private int row, column;
+  private HashMap<Character, String> terrainMapping;
+
+  public Player(int row, int column) {
+    this.row = row;
+    this.column = column;
+    initializeTerrainMapping();
+  }
+
+  public void followPath(List<int[]> path) {
+    for (int[] step : path) {
+      this.row = step[0];
+      this.column = step[1];
+    }
+  }
+
+  public void setPosition(int x, int y) {
+    this.row = x;
+    this.column = y;
+  }
+
+  private void initializeTerrainMapping() {
+    terrainMapping = new HashMap<>();
+    terrainMapping.put(' ', "1");
+    terrainMapping.put('.', "0");
+    terrainMapping.put('-', "1");
+    terrainMapping.put('=', "0");
+    terrainMapping.put('|', "1");
+    terrainMapping.put(':', "0");
+    terrainMapping.put('+', "0");
+    terrainMapping.put('O', "0");
+    terrainMapping.put('#', "0");
+    terrainMapping.put('*', "0");
+    terrainMapping.put('$', "0");
+    terrainMapping.put('X', "0");
+    terrainMapping.put('%', "0");
+    terrainMapping.put('_', "0");
+    terrainMapping.put('~', "1");
+    terrainMapping.put('B', "0");
+    terrainMapping.put('P', "*");
+    terrainMapping.put('D', "0");
+    terrainMapping.put('S', "0");
+    terrainMapping.put('C', "0");
+  }
+
+  public void move(int direction, String[] map, GameWorld world) {
+    int oldRow = row;
+    int oldColumn = column;
+
+    switch (direction) {
+      case 0:
+        row--;
+        break;
+      case 1:
+        row++;
+        break;
+      case 2:
+        column--;
+        break;
+      case 3:
+        column++;
+        break;
+    }
+
+    if (row < 0 || row >= map.length || column < 0 || column >= map[0].length()
+        || !isWalkable(map[row].charAt(column))) {
+      row = oldRow;
+      column = oldColumn;
+    }
+  }
+
+  public void moveTo(int newX, int newY) {
+    this.row = newX;
+    this.column = newY;
+}
+
+  private boolean isWalkable(char terrain) {
+
+    return terrainMapping.getOrDefault(terrain, "1").equals("0");
+  }
+
+  public int getRow() {
+    return row;
+  }
+
+  public int getColumn() {
+    return column;
+  }
+}
---
+++ entregas/andresAlarcon/ev5/src/Renderer.java
@@ -0,0 +1,73 @@
+public class Renderer {
+
+  private static final int DAWN = 6;
+  private static final int DUSK = 18;
+
+  public void printWorld(String[] map, Player player, GameWorld world, Visualizer visualizer) {
+    clearScreen();
+    printSky(world, visualizer);
+    int viewPort = world.getViewPort();
+    int visualRange = world.getVisualRange();
+    int rowStart = Math.max(0, player.getRow() - viewPort);
+    int rowEnd = Math.min(map.length, player.getRow() + viewPort + 1);
+    int colStart = Math.max(0, player.getColumn() - viewPort);
+    int colEnd = Math.min(map[0].length(), player.getColumn() + viewPort + 1);
+
+    for (int i = rowStart; i < rowEnd; i++) {
+      for (int j = colStart; j < colEnd; j++) {
+        double distance = Math.sqrt(Math.pow(i - player.getRow(), 2) + Math.pow(j - player.getColumn(), 2));
+        if (distance <= visualRange) {
+          if (i == player.getRow() && j == player.getColumn()) {
+            System.out.print(visualizer.mapElement('P', world.getVisualizationMode()));
+          } else {
+            char elem = map[i].charAt(j);
+            System.out.print(visualizer.mapElement(elem, world.getVisualizationMode()));
+          }
+        } else {
+          System.out.print(GRAY_BACKGROUND + "   " + RESET);
+        }
+      }
+      System.out.println();
+    }
+    printStatus(player, world);
+  }
+
+  private void printSky(GameWorld world, Visualizer visualizer) {
+    double time = world.getTime();
+    int viewPortSize = world.getViewPort() * 2 + 1;
+    int sunPosition = calculateSunPosition(time, viewPortSize);
+
+    for (int i = 0; i < viewPortSize; i++) {
+      if (time > DAWN && time <= DUSK && i == sunPosition) {
+        System.out.print(visualizer.mapElement('S', world.getVisualizationMode()));
+      } else {
+        System.out.print(visualizer.mapElement('C', world.getVisualizationMode()));
+      }
+    }
+    System.out.println();
+    System.out.println();
+
+  }
+
+  private int calculateSunPosition(double time, int viewPortSize) {
+    if (time < DAWN || time > DUSK) {
+      return -1;
+    }
+    return (int) ((time - DAWN) / (DUSK - DAWN) * viewPortSize) % viewPortSize;
+  }
+
+  private void printStatus(Player player, GameWorld world) {
+    System.out.println(
+        "Time: [" + (int) world.getTime() + "] / Position: (" + player.getRow() + "," + player.getColumn()
+            + ") / Skin: ("
+            + world.getVisualizationMode() + ")");
+  }
+
+  private void clearScreen() {
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
+  }
+
+  public static final String GRAY_BACKGROUND = "\033[100m";
+  public static final String RESET = "\033[0m";
+}
