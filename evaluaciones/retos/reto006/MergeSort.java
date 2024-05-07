ac8ac6b0708104f7874bc9104983e499d0c4b047
luciaZama <lzamanillol@gmail.com>
2024-05-07 21:11:11 +0200

    MergeSort terminado

--- evaluaciones/retos/reto006/MergeSort.java
+++ evaluaciones/retos/reto006/MergeSort.java
@@ -4,23 +4,24 @@ public class MergeSort {
 // Método para dividir y fusionar el array
 public static void sort(int[] array, int left, int right) {
     if (left < right) {
-        // Encuentra el punto medio del vector.
         int middle = (left + right) / 2;
+        System.out.println("Punto medio del vector: " + middle);
 
-        // Divide la primera y segunda mitad (llamada recursiva).
+        System.out.println("Dividiendo la primera mitad del vector...");
         sort(array, left, middle);
+        System.out.println("Dividiendo la segunda mitad del vector...");
         sort(array, middle + 1, right);
 
-        // Une las mitades.
+        System.out.println("Fusionando las mitades del vector...");
         merge(array, left, middle, right);
     }
 }
 
 // Método para fusionar las dos mitades.
 private static void merge(int[] array, int left, int middle, int right) {
-    // Tamaños de los subarrays temporales.
     int n1 = middle - left + 1;
     int n2 = right - middle;
+    System.out.println("Tamaño del subarray izquierdo: " + n1 + "; Tamaño del subarray derecho: " + n2);
 
     // Subarrays temporales.
     int[] L = new int[n1];
@@ -29,9 +30,11 @@ private static void merge(int[] array, int left, int middle, int right) {
     // Copia los datos a los arrays temporales.
     for (int i = 0; i < n1; i++) {
         L[i] = array[left + i];
+        System.out.println("Copiando " + array[left + i] + " en el subarray izquierdo");
     }
     for (int j = 0; j < n2; j++) {
         R[j] = array[middle + 1 + j];
+        System.out.println("Copiando " + array[middle + 1 + j] + " en el subarray derecho");
     }
 
     // Índices iniciales de los subarrays.
@@ -40,7 +43,7 @@ private static void merge(int[] array, int left, int middle, int right) {
     // Índice inicial del subarray mezclado.
     int k = left;
 
-    // Ordenamiento y mezcla.
+    System.out.println("Ordenando y mezclando los arrays...");
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
             array[k] = L[i];
@@ -52,16 +55,16 @@ private static void merge(int[] array, int left, int middle, int right) {
         k++;
     }
 
-    // Copiar los elementos restantes de L, si los hay.
     while (i < n1) {
         array[k] = L[i];
+        System.out.println("Copiando " + L[i] + " del subarray izquierdo al subarray mezclado");
         i++;
         k++;
     }
 
-    // Copiar los elementos restantes de R, si los hay.
     while (j < n2) {
         array[k] = R[j];
+        System.out.println("Copiando " + R[j] + " del subarray derecho al subarray mezclado");
         j++;
         k++;
     }
