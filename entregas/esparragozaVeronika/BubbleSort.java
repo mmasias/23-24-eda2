91bc25a66f26cc6dcd4c160f6b4fb3e9cbbacd93
Veronika Esparragoza <vesparragozam@gmail.com>
2024-05-08 17:54:04 +0200

    Añadiendo los primeros ejecicios sin resolver

---
+++ entregas/esparragozaVeronika/BubbleSort.java
@@ -0,0 +1,30 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] data = {64, 34, 25, 12, 22, 11, 90};
+        sort(data);
+        System.out.println("Array ordenado:");
+        for (int i : data) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void sort(int[] array) {
+        boolean sorted = false;
+        int passIndex = 0; // Para reducir el alcance del barrido en cada pasada
+        while (!sorted) {
+            sorted = true; // Se asume que el arreglo está ordenado
+            for (int j = 0; j < array.length - 1 - passIndex; j++) {
+                if (array[j] > array[j + 1]) {
+                    // Intercambio de elementos
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    sorted = false; // Se encontró un desorden, se continua
+                }
+            }
+            passIndex++;
+        }
+    }
+
+
+}
