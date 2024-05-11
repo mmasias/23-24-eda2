8ec70833547d069502cdd81bb37be804c9be8ea8
jorge-85015 <jorge85015@gmail.com>
2024-05-11 13:30:21 +0200

    feature/insert-all-sort-examples-for-process-tracking

---
+++ entregas/quinonezJorge/BubbleSort.java
@@ -0,0 +1,24 @@
+public class BubbleSort {
+    public static void sort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) { // i es el número de pasadas completas
+            for (int j = 0; j < n - 1; j++) { // j es el índice para comparar elementos adyacentes
+                if (array[j] > array[j + 1]) {
+                    // Intercambio de elementos
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] data = {64, 34, 25, 12, 22, 11, 90};
+        sort(data);
+        System.out.println("Array ordenado:");
+        for (int i : data) {
+            System.out.print(i + " ");
+        }
+    }
+}
---
+++ entregas/quinonezJorge/InsertionSort.java
@@ -0,0 +1,22 @@
+public class InsertionSort {
+  public static void sort(int[] array) {
+    for (int i = 1; i < array.length; i++) {
+      int current = array[i];
+      int j = i - 1;
+      while (j >= 0 && array[j] > current) {
+        array[j + 1] = array[j]; // Desplaza el elemento hacia la derecha
+        j--;
+      }
+      array[j + 1] = current; // Inserta el elemento en su lugar correcto
+    }
+  }
+
+  public static void main(String[] args) {
+    int[] data = { 22, 11, 99, 88, 9, 7, 42 };
+    sort(data);
+    System.out.println("Array ordenado:");
+    for (int value : data) {
+      System.out.print(value + " ");
+    }
+  }
+}
