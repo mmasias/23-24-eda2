bfe941183ed2254c1ff9e2a19d655c4bcc231348
LavinAguado <daniellavinaguado00@gmail.com>
2024-05-14 21:05:19 +0200

    commit reto 7 pt1

---
+++ entregas/lavinDaniel/reto007/RetoEDA.java
@@ -0,0 +1,20 @@
+package entregas.lavinDaniel.reto007;
+class RetoEDA {
+
+    private Baraja baraja;
+
+    public static void main(String[] args) {
+
+        Baraja baraja = new Baraja();
+
+        //
+
+        //
+        Carta carta;
+        do {
+            carta = baraja.sacar();
+            carta.voltear();
+            carta.mostrar();
+        } while (!baraja.vacia());
+    }
+}
