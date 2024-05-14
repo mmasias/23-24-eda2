ade8bbc12f05350b55166746aecce714cb95bff4
jorgeog25 <jrotero258@gmail.com>
2024-05-14 15:38:11 +0200

    Ordena por numeros y palos

--- entregas/oteroJorge/reto007/src/Ordenar.java
+++ entregas/oteroJorge/reto007/src/Ordenar.java
@@ -27,14 +27,18 @@ public class Ordenar {
     public static void ordenarPorNumero(Carta[] cartas) {
         int n = cartas.length;
         boolean swapped;
-        
+    
         do {
             swapped = false;
             for (int i = 0; i < n - 1; i++) {
+                // Obtener información de las cartas actuales
+                int palo1 = cartas[i].getPalo();
                 int numero1 = cartas[i].getNumero();
+                int palo2 = cartas[i + 1].getPalo();
                 int numero2 = cartas[i + 1].getNumero();
     
-                if (numero1 > numero2) {
+                // Comparar números y palos para ordenar
+                if (numero1 > numero2 || (numero1 == numero2 && palo1 > palo2)) {
                     Carta temp = cartas[i];
                     cartas[i] = cartas[i + 1];
                     cartas[i + 1] = temp;
