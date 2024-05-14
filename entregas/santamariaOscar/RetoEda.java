4479d885f53ccc35b1a3b4408a9a737f234931b8
oscarsantasanchez <oscar.santamaria@alumnos.uneatlantico.es>
2024-05-14 14:36:42 +0200

    programa inicial del reto

---
+++ entregas/santamariaOscar/RetoEda.java
@@ -0,0 +1,24 @@
+package entregas.santamariaOscar;
+
+
+
+
+class RetoEDA {
+
+    private Baraja baraja;
+
+    public static void main(String[] args) {
+
+        Baraja baraja = new Baraja();
+
+        //
+       
+        //
+        Carta carta;
+        do {
+            carta = baraja.sacar();
+            carta.voltear();
+            carta.mostrar();
+        } while (!baraja.vacia());
+    }
+}
