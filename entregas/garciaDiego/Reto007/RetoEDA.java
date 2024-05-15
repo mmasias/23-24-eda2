87dc42b4111e9aa2d361c81a145c31c26aff9f6c
Dievexx <diego30sscc@gmail.com>
2024-05-15 19:26:24 +0200

    Pequeña limpieza de codigo

--- entregas/garciaDiego/Reto007/RetoEDA.java
+++ entregas/garciaDiego/Reto007/RetoEDA.java
@@ -1,6 +1,3 @@
-//import java.util.Comparator;
-import java.util.Scanner;
-
 class RetoEDA {
 
     private Baraja baraja;
@@ -10,27 +7,28 @@ class RetoEDA {
         Baraja baraja = new Baraja();
 
         //
-        
+
         Carta carta1;
-        Carta[] cartasOrdenadasNumero = new Carta[52]; 
+        Carta[] cartasOrdenadasNumero = new Carta[52];
         boolean sorted = false;
         int passIndex = 0;
 
-        for(int i=0;i<cartasOrdenadasNumero.length;i++){
+        for (int i = 0; i < cartasOrdenadasNumero.length; i++) {
             carta1 = baraja.sacar();
             cartasOrdenadasNumero[i] = carta1;
         }
 
         while (!sorted) {
-            sorted = true; // Se asume que el arreglo está ordenado
+            sorted = true;
             for (int j = 0; j < cartasOrdenadasNumero.length - 1 - passIndex; j++) {
                 if (cartasOrdenadasNumero[j].getNumero() > cartasOrdenadasNumero[j + 1].getNumero()) {
-                    System.out.println("Intercambio de los elementos: " + cartasOrdenadasNumero[j] + " pasa a la derecha de " +  cartasOrdenadasNumero[j + 1]);
+                    System.out.println("Intercambio de los elementos: " + cartasOrdenadasNumero[j]
+                            + " pasa a la derecha de " + cartasOrdenadasNumero[j + 1]);
                     Carta temp = cartasOrdenadasNumero[j];
                     cartasOrdenadasNumero[j] = cartasOrdenadasNumero[j + 1];
                     cartasOrdenadasNumero[j + 1] = temp;
                     System.out.println("Continua porque esta en desorden");
-                    sorted = false; 
+                    sorted = false;
                 }
             }
             passIndex++;
@@ -41,29 +39,28 @@ class RetoEDA {
 
             baraja.poner(cartasOrdenadasNumero[i]);
         }
-        
+
         Carta[] cartasOrdenadasPalo = new Carta[52];
         sorted = false;
         passIndex = 0;
         Baraja baraja2 = new Baraja();
 
-
-
-        for(int i=0;i<cartasOrdenadasPalo.length;i++){
+        for (int i = 0; i < cartasOrdenadasPalo.length; i++) {
             carta1 = baraja2.sacar();
             cartasOrdenadasPalo[i] = carta1;
         }
 
         while (!sorted) {
-            sorted = true; // Se asume que el arreglo está ordenado
+            sorted = true;
             for (int j = 0; j < cartasOrdenadasPalo.length - 1 - passIndex; j++) {
                 if (cartasOrdenadasPalo[j].getPalo() > cartasOrdenadasPalo[j + 1].getPalo()) {
-                    System.out.println("Intercambio de los elementos: " + cartasOrdenadasPalo[j] + " pasa a la derecha de " +  cartasOrdenadasPalo[j + 1]);
+                    System.out.println("Intercambio de los elementos: " + cartasOrdenadasPalo[j]
+                            + " pasa a la derecha de " + cartasOrdenadasPalo[j + 1]);
                     Carta temp = cartasOrdenadasPalo[j];
                     cartasOrdenadasPalo[j] = cartasOrdenadasPalo[j + 1];
                     cartasOrdenadasPalo[j + 1] = temp;
                     System.out.println("Continua porque esta en desorden");
-                    sorted = false; 
+                    sorted = false;
                 }
             }
             passIndex++;
@@ -74,7 +71,6 @@ class RetoEDA {
             baraja2.poner(cartasOrdenadasPalo[i]);
         }
 
-        //
         Carta carta;
         System.out.println("\n");
         System.out.println("\u001B[34mBARAJA ORDENADA POR NUMEROS\u001B[0m");
@@ -86,12 +82,13 @@ class RetoEDA {
 
         System.out.println("\n");
         System.out.println("\u001B[34mBARAJA ORDENADA POR PALOS\u001B[0m");
+
+        //
         do {
             carta = baraja2.sacar();
             carta.voltear();
             carta.mostrar();
         } while (!baraja2.vacia());
 
-        
     }
 }
