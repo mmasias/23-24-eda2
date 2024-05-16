076d8c5a9772af48ba11aed16c66a7cacead9e93
Sergio Mayen <sergio.mayen@ct.uneatlantico.es>
2024-05-16 22:19:01 +0200

    Avance2: Realización de un ordenamiento por orden de corazones, treboles, picas y diamantes

--- entregas/mayenSergio/Reto_007/RetoEDA.java
+++ entregas/mayenSergio/Reto_007/RetoEDA.java
@@ -1,5 +1,8 @@
 package entregas.mayenSergio.Reto_007;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class RetoEDA {
 
     private Baraja baraja;
@@ -8,14 +11,52 @@ public class RetoEDA {
 
         Baraja baraja = new Baraja();
 
-        //
+        Carta[] cartas = new Carta[52];
+        int index = 0;
 
-        //
-        Carta carta;
-        do {
-            carta = baraja.sacar();
-            carta.voltear();
-            carta.mostrar();
-        } while (!baraja.vacia());
+        while (!baraja.vacia()) {
+            cartas[index] = baraja.sacar();
+            index++;
+        }
+
+        Arrays.sort(cartas, 0, index, new Comparator<Carta>() {
+            @Override
+            public int compare(Carta c1, Carta c2) {
+                int palo1 = getOrdenPalo(c1);
+                int palo2 = getOrdenPalo(c2);
+
+                if (palo1 != palo2) {
+                    return Integer.compare(palo1, palo2);
+                } else {
+                    return Integer.compare(getNumero(c2), getNumero(c1));
+                }
+            }
+
+            private int getOrdenPalo(Carta carta) {
+                if (carta.igualPalo(new Carta(2, 0))) {
+                    return 0;
+                } else if (carta.igualPalo(new Carta(1, 0))) {
+                    return 1;
+                } else if (carta.igualPalo(new Carta(0, 0))) {
+                    return 2;
+                } else {
+                    return 3;
+                }
+            }
+
+            private int getNumero(Carta carta) {
+                for (int i = 0; i < 13; i++) {
+                    if (carta.siguiente(new Carta(0, i))) {
+                        return i;
+                    }
+                }
+                return 12;
+            }
+        });
+
+        for (int i = 0; i < index; i++) {
+            cartas[i].voltear();
+            cartas[i].mostrar();
+        }
     }
 }
