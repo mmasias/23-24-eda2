be9cf28067fd1bfeec6db0ed1cacd48e81a8912f
TheMoys <diegomoys01@gmail.com>
2024-05-18 10:50:46 +0200

    Añadiendo metodo ordenador de cartas

--- entregas/diegoMoys/reto007/OrdenadorCartas.java
+++ entregas/diegoMoys/reto007/OrdenadorCartas.java
@@ -1,3 +1,46 @@
-public class OrdenadorCartas {
-    
+class OrdenadorCartas {
+
+    public static void sort(Carta[] cartas, boolean ordenarPorPalo) {
+        boolean swapped;
+        int passIndex = 0;
+        do {
+            swapped = false;
+            for (int j = 0; j < cartas.length - 1 - passIndex; j++) {
+                if (ordenarPorPalo) {
+                    if (compararPorPalo(cartas[j], cartas[j + 1]) > 0) {
+                        intercambiar(cartas, j, j + 1);
+                        swapped = true;
+                    }
+                } else {
+                    if (compararPorNumero(cartas[j], cartas[j + 1]) > 0) {
+                        intercambiar(cartas, j, j + 1);
+                        swapped = true;
+                    }
+                }
+            }
+            passIndex++;
+        } while (swapped);
+    }
+
+    private static int compararPorPalo(Carta carta1, Carta carta2) {
+        if (carta1.getPalo() == carta2.getPalo()) {
+            return carta1.getNumero() - carta2.getNumero();
+        } else {
+            return carta1.getPalo() - carta2.getPalo();
+        }
+    }
+
+    private static int compararPorNumero(Carta carta1, Carta carta2) {
+        if (carta1.getNumero() == carta2.getNumero()) {
+            return carta1.getPalo() - carta2.getPalo();
+        } else {
+            return carta1.getNumero() - carta2.getNumero();
+        }
+    }
+
+    private static void intercambiar(Carta[] cartas, int i, int j) {
+        Carta temp = cartas[i];
+        cartas[i] = cartas[j];
+        cartas[j] = temp;
+    }
 }
--- entregas/diegoMoys/reto007/RetoEDA.java
+++ entregas/diegoMoys/reto007/RetoEDA.java
@@ -1,3 +1,59 @@
+import java.util.Scanner;
 public class RetoEDA {
-    
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String continuar;
+
+        do {
+            Baraja baraja = new Baraja(); 
+            int opcion = obtenerOpcion(scanner);
+
+            boolean ordenarPorPalo = opcion == 1;
+
+            Carta[] cartas = extraerCartasDeBaraja(baraja);
+
+            OrdenadorCartas.sort(cartas, ordenarPorPalo);
+
+            mostrarCartas(cartas);
+
+            System.out.println("Presiona 'q' para salir o cualquier otra tecla para continuar...");
+            continuar = scanner.next();
+
+        } while (!continuar.equalsIgnoreCase("q"));
+
+        scanner.close();
+    }
+
+    private static int obtenerOpcion(Scanner scanner) {
+        int opcion;
+        do {
+            System.out.println("¿Cómo deseas ordenar las cartas?");
+            System.out.println("1. Por palo");
+            System.out.println("2. Por número");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Opción inválida. Intenta de nuevo.");
+                scanner.next();
+            }
+            opcion = scanner.nextInt();
+        } while (opcion < 1 || opcion > 2);
+        return opcion;
+    }
+
+    private static Carta[] extraerCartasDeBaraja(Baraja baraja) {
+        Carta[] cartas = new Carta[Baraja.TOTAL_CARTAS];
+        for (int i = 0; i < Baraja.TOTAL_CARTAS; i++) {
+            cartas[i] = baraja.sacar();
+        }
+        return cartas;
+    }
+
+    private static void mostrarCartas(Carta[] cartas) {
+        for (Carta carta : cartas) {
+            carta.voltear();
+            carta.mostrar();
+            System.out.println();
+        }
+    }
 }
